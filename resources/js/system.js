f37d6ed211df6c65af0045e68e3c6f56da5ed3b0
Woloja <d7E7Y79s3Pz5>
2020-02-09 16:47:53 +0200

    remove old dep

--- resources/js/system.js
+++ resources/js/system.js
@@ -1,14 +1,3 @@
-//css-libs
-import "./_system-classes/css/system.css"
-import "./_system-classes/css/sweetalert2.css"
-
-//js-libs
-import Swal from 'sweetalert2/dist/sweetalert2'
-const queryString = require('query-string');
-
-
-global.queryString = queryString;
-global.Swal = Swal;
 
 global.isDomainLink = function (link) {
     return link.indexOf(location.origin) === 0 || link.startsWith("/");
